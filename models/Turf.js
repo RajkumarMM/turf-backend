a85d4fdbc79efc2cc189380fc9b303442a681caf
RajkumarMM <mmraj816@gmail.com>
2025-01-24 19:27:05 +0530

    turf model slote type changed

--- models/Turf.js
+++ models/Turf.js
@@ -6,14 +6,12 @@ const turfSchema = new mongoose.Schema(
     name: { type: String, required: true },
     location: { type: String, required: true },
     price: { type: String, required: true },
-    slots: {
-      type: Map,
-      of: {
+    slots: [
+      {
         time: { type: String, required: true },
         isBooked: { type: Boolean, required: true },
       },
-      required: true,
-    },
+    ], // Define slots as an array of objects
     ownerId: { 
       type: mongoose.Schema.Types.ObjectId, 
       ref: "Owner", // Reference to the Owner model
