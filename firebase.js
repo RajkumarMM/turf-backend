a007c3e5232fb413a74cceb59b032cbd392ba6c6
RajkumarMM <mmraj816@gmail.com>
2025-03-25 11:22:20 +0530

    firbase key solved

--- firebase.js
+++ firebase.js
@@ -1,10 +1,24 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
+import dotenv from "dotenv";
 
-// Read the Firebase service account key file
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("./config/turf-86bf0-firebase-adminsdk-fbsvc-12e02ed181.json", import.meta.url))
-);
+dotenv.config();
+
+// Parse private key correctly
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: privateKey,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
